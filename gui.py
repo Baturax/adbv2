2ee5f1bdfe28f563734213aaf7a9eab4d382d765
baturax <batuhanakkurt000@gmail.com>
2024-04-10 01:20:13 +0300

    afds

--- gui.py
+++ gui.py
@@ -1,13 +1,14 @@
 import tkinter as tk
 import subprocess
+from turtle import left
 
 #const
 adb_download = "https://dl.google.com/android/repository/platform-tools-latest-linux.zip"
 zip_file = "adb.zip"
 extract_folder = "adbb"
 
-
 #def
+#downlaod adb
 def download_adb():
     # Dosyayı indirme
     download_process = subprocess.Popen(["./wget", "-N", adb_download, "-O", zip_file])
@@ -19,12 +20,18 @@ def download_adb():
         print("downloaded successfully")
         subprocess.run(["rm", zip_file])
 
+#adb tools
+def adb_tools():
+    subprocess.run(["adb", "devices"])
+
+
 #tkinter
 main = tk.Tk()
 
 button = tk.Button(main, text="Download ADB", bg="white", fg="#282C34", command=download_adb)
-button.pack()
+button.pack(anchor="ne")
 
 # configs
+main.geometry("400x300")
 main.tk_setPalette("#282C34")
 main.mainloop()
