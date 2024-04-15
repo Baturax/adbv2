e45852a0e30e9732bca8bdc4d9331f5185345908
baturax <batuhanakkurt000@gmail.com>
2024-04-16 01:48:00 +0300

    sd

--- gui.py
+++ gui.py
@@ -1,7 +1,5 @@
-import tkinter as tk
-from tkinter import ttk, filedialog, Label, Button, Tk
 import subprocess
-
+from tkinter import filedialog, Label, Button, Tk
 
 adb_download = "https://dl.google.com/android/repository/platform-tools-latest-linux.zip"
 zip_file = "adb.zip"
@@ -9,19 +7,20 @@ extract_folder = "adb"
 adb = "./adb/platform-tools/adb"
 device_folder = "device/"
 
-#run android-file-transfer
-print("slect your phone's file transfer, set your phone to file transfer mode and enable usb debug mode in developer options.")
+# run android-file-transfer
+print(
+    "slect your phone's file transfer, set your phone to file transfer mode and enable usb debug mode in developer options.")
 subprocess.run(["aft-mtp-mount", device_folder])
 
 
-#def
+# def
 
-#downlaod adb
+# downlaod adb
 def download_adb():
     # Dosyayı indirme
     download_process = subprocess.Popen(["wget", "-N", adb_download, "-O", zip_file])
-    download_process.communicate() 
-    
+    download_process.communicate()
+
     if download_process.returncode == 0:
         unzip_process = subprocess.Popen(["unzip", "-o", zip_file, "-d", extract_folder])
         unzip_process.communicate()
@@ -29,41 +28,47 @@ def download_adb():
         subprocess.run(["rm", zip_file])
 
 
-#adb tools
-#adb devices
+# adb tools
+# adb devices
 def adb_devices():
     subprocess.run([adb, "devices"])
 
-#adb push
-def push ():
+
+# adb push
+def push():
     print("Select file to push")
     pushedfile = filedialog.askopenfilename(initialdir="~/")
     print("Selected file: " + pushedfile)
     wherepushed = filedialog.askdirectory(initialdir=device_folder)
     print("The file will be pushed to: " + wherepushed)
-    subprocess.run(["cp",  pushedfile, wherepushed])
-#adb pull
+    subprocess.run(["cp", pushedfile, wherepushed])
+
+
+# adb pull
 def pull():
     print("Select file to pull")
     pulledfile = filedialog.askopenfilename(initialdir=device_folder)
     print("Selected file: " + pulledfile)
     wherepulled = filedialog.askdirectory(initialdir="~/")
     print("The file will be pulled to: " + wherepulled)
-    subprocess.run(["cp",  pulledfile, wherepulled])
+    subprocess.run(["cp", pulledfile, wherepulled])
 
-#adb reboot
+
+# adb reboot
 def rebootdevice():
     subprocess.run([adb, "reboot"])
+
+
 def rebootrecovery():
     subprocess.run([adb, "reboot", "recovery"])
+
+
 def rebootbootloader():
     subprocess.run([adb, "reboot", "bootloader"])
 
 
-
-#"tkinter"
+# tkinter
 root = Tk()
-
 
 # This is the section of code which creates the main window
 root.geometry('700x500')
@@ -74,19 +79,20 @@ Label(root, text='ADB Tools', bg='#8B4513', font=('arial', 14, 'normal')).place(
 
 Button(root, text='ADB Devices', bg='#FFFFFF', font=('arial', 12, 'normal'), command=adb_devices).place(x=2, y=40)
 
-Button(root, text='Push File', bg='#FFFFFF',font=('arial', 12, 'normal'), command=push).place(x=2, y=75)
+Button(root, text='Push File', bg='#FFFFFF', font=('arial', 12, 'normal'), command=push).place(x=2, y=75)
 
 Button(root, text='Pull Folder', bg='#FFFFFF', font=('arial', 12, 'normal'), command=pull).place(x=2, y=110)
 
-#yok
+# yok
 Label(root, text='ADB Download', bg='#8B4513', font=('arial', 14, 'normal')).place(x=2, y=160)
 
 Button(root, text='Restart Device', bg='#FFFFFF', font=('arial', 12, 'normal'), command=rebootdevice).place(x=2, y=195)
 
-Button(root, text='Reboot Recovery', bg='#FFFFFF', font=('arial', 12, 'normal'), command=rebootrecovery).place(x=2, y=230)
+Button(root, text='Reboot Recovery', bg='#FFFFFF', font=('arial', 12, 'normal'), command=rebootrecovery).place(x=2,
+                                                                                                               y=230)
 
-Button(root, text='Reboot Bootloader', bg='#FFFFFF', font=('arial', 12, 'normal'), command=rebootbootloader).place(x=2, y=265)
-
+Button(root, text='Reboot Bootloader', bg='#FFFFFF', font=('arial', 12, 'normal'), command=rebootbootloader).place(x=2,
+                                                                                                                   y=265)
 
 Button(root, text='Download ADB', bg='#FFFFFF', font=('arial', 12, 'normal'), command=download_adb).place(x=550, y=5)
 
